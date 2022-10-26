ca8c57d2632e2ccc69e4882621d4ff54bdbc6eae
dmnot <tomatsuper1701@gmail.com>
2022-10-27 01:00:32 +0500

    Cats Arrays Example

--- src/ru/synergy/Cats.java
+++ src/ru/synergy/Cats.java
@@ -1,5 +1,26 @@
 package ru.synergy;
 
 public class Cats {
+   //-----------------------
+     private String name;
+   //-----------------------
 
+    public Cats(String name) {
+        this.name = name;
+    }
+    //------------------------
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Cats{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -1,8 +1,33 @@
 package ru.synergy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Cats[] cat = new Cats[3]; //Создание массива для трех элементов
+        //------------------------------------------
+
+        cat[0] = new Cats("Томас");
+        cat[1] = new Cats("Бегемот");
+        cat[2] = new Cats("Филипп Маркович");
+        //------------------------------------------
+        //cat[1] = null;
+        System.out.println(Arrays.toString(cat));
+        //------------------------------------------
+        // Динамический массив
+        ArrayList<Cats> catsList = new ArrayList(); // Создание динамического массива
+        for(Cats cats : cat){ // Создание цикла который берет значения из массива
+            // И закидывает их в динамический
+            catsList.add(cats); // Добавление элементов в динамический массив
+        }
+        //-----------------------------------------
+        catsList.add( new Cats("Бегемот 2")); // Добавление элемента в динамический массив
+        System.out.println(catsList.toString()); //
+        //-----------------------------------------
+
     }
 }
